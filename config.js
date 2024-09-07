d8de08297f77cd485fc2bdc0e2218750b7d3aeac
VISHWA796 <vishwa.dinal123@gmail.com>
2024-09-07 15:27:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split("+94763214503")
+global.owner = process.env.OWNER_NUMBER.split("94763214503")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
 global.email = 'sam@sec.live'
